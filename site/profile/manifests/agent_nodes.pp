b0f0774d41025fb9298ba9e9a718114103d40495
Yuriy Pyvovarov <yuriy.pyvovarov@gmail.com>
2018-02-14 11:55:38 +0200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,12 +2,13 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node { 'web.puppet.vm': }
   dockeragent::node { 'db.puppet.vm': }
-  host { 'web.puppet.vm':
+  dockeragent::node { 'minetest.puppet.vm': }
+  host {‘web.puppet.vm’:
     ensure => present,
-    ip => '172.18.0.2',
+    ip => ‘172.18.0.2’,
   }
-  host { 'db.puppet.vm':
+  host {‘db.puppet.vm’:
     ensure => present,
-    ip => '172.18.0.3',
+    ip => ‘172.18.0.3’,
   }
 }
